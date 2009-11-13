c8075ef90e90565fa1fa774254326156edbc1f12
bshaffer <bshaffer@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-13 16:25:44 -0600

    cleaning code up, replacing hard tabs w/ soft tabs
    git-svn-id: http://svn.symfony-project.com/plugins/csDoctrineActAsAttachablePlugin/trunk@23866 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/listener/Attachable.php
+++ lib/listener/Attachable.php
@@ -17,27 +17,16 @@ class Doctrine_Template_Listener_Attachable extends Doctrine_Record_Listener
    */
   public function preSave(Doctrine_Event $event)
   {
-		$object = $event->getInvoker();
-		//If the object is new, and attachments are set, save them
-		if(!$object->getId() &&  isset($object['Attachments']))
-		{
-			foreach ($object['Attachments'] as $attachment) 
-			{
-				$object->addAttachment(new Attachment($attachment));
-			}
-		}
-    //$object = $event->getInvoker();
-    //
-    //$added_attachment = Doctrine_Template_Attachable::get_attachment($object);
-    //
-    //foreach ($added_attachment as $attachment)
-    //{
-    //  $attachment = new Attachment;
-    //  $attachment->setClass(get_class($object));
-    //  $attachment->setObjectId($object->getId());
-    //  $attachment->setAttachment($attachment);
-    //  $attachment->save();
-    //}
+    $object = $event->getInvoker();
+    
+    //If the object is new, and attachments are set, save them
+    if(!$object->getId() &&  isset($object['Attachments']))
+    {
+      foreach ($object['Attachments'] as $attachment) 
+      {
+        $object->addAttachment(new Attachment($attachment));
+      }
+    }
   }
 
 
--- lib/template/Attachable.php
+++ lib/template/Attachable.php
@@ -1,8 +1,7 @@
 <?php
 class Doctrine_Template_Attachable extends Doctrine_Template
 {
-  protected $_options = array('types' => array(),
-															'attachableModels' => array());
+  protected $_options = array('types' => array());
   
 
 
@@ -28,109 +27,110 @@ class Doctrine_Template_Attachable extends Doctrine_Template
      $this->addListener(new Doctrine_Template_Listener_Attachable());
   }
 
-	public function getAttachmentsQuery()
-	{
-		$object = $this->getInvoker();
-		$query = Doctrine::getTable('Attachment')->createQuery();
-		$query->addWhere('Attachment.object_class = ?', get_class($object))
-					->addWhere('Attachment.object_id = ?', $object->getId());
-					
-		return $query;
-	}
- 	public function getAttachments()
-	{
-		$object = $this->getInvoker();
-		return $object->getAttachmentsQuery()->execute();
-	}
-	public function setAttachments($attachments)
-	{
-		foreach ($attachments as $attachment) 
-		{
-			$this->addAttachment($attachment);
-		}
+  public function getAttachmentsQuery()
+  {
+    $object = $this->getInvoker();
+    $query = Doctrine::getTable('Attachment')->createQuery();
+    $query->addWhere('Attachment.object_class = ?', get_class($object))
+          ->addWhere('Attachment.object_id = ?', $object->getId());
+          
+    return $query;
   }
-	public function hasAttachments()
-	{
-		return ($this->getAttachments()->count() > 0);
-	}
-	public function hasAttachmentsOfType($type)
-	{
-		return ($this->getAttachmentsByType($type)->count() > 0);
-	}
-	public function addAttachment($attachment)
-	{
-		$object = $this->getInvoker();
-		$attachment->setObjectClass(get_class($object));
-		if(!$object['id'])
-		{
-			$object->save();
-		}
-		$attachment->setObjectId($object->getId());
-		$attachment->save();	
-	}
-	
-	public function getAttachmentsByType($type)
-	{
-		if(in_array($type, $this->_options['types']))
-		{
-			return Doctrine::getTable('Attachment')
-									->createQuery()
-									->where('object_class = ?', get_class($this->getInvoker()))
-									->andWhere('object_id = ?', $this->getInvoker()->getId())
-									->andWhere('type = ?', strtolower($type))
-									->execute();
-		}			
-		$table = strtolower($type) == 'other' ? 'Attachment' : sfInflector::classify($type.'_attachment');
-		return new Doctrine_Collection($table);
-	}
-	public function getSupportedAttachmentTypes()
-	{
-		return $this->getAttachableConfig('types');
-	}
-	public function getAttachableConfig($index = null)
-	{
-		if($index)
-		{
-			return $this->_options[$index];
-		}
-		return $this->_options;
-	}
-	public function getVideoAttachments()
-	{
-		return $this->getAttachmentsByType('Video');
-	}
-	public function getImageAttachments()
-	{
-		return $this->getAttachmentsByType('Image');
-	}
-	public function getDocumentAttachments()
-	{
-		return $this->getAttachmentsByType('Document');
-	}
-	public function getOtherAttachments()
-	{
-		return $this->getAttachmentsByType('Other');
-	}
-	public function getModelAttachments()
-	{
-		return $this->getAttachmentsByType('Model');
-	}
-	//This method doesn't work for some reason!
-	public function __call($method, $arguments)
-	{
-		// try
-		// {
-			// parent::__call($method, $arguments);
-		// }
-		// catch(Exception $e)
-		// {
-			foreach ($this->_options['types'] as $type) 
-			{
-				if(sfInflector::camelize('get_'.$type.'_attachments') == $method)
-				{
-					return $this->getAttachmentsByType($type);
-				}
-			}
-	  // }
-	}
+  public function getAttachments()
+  {
+    $object = $this->getInvoker();
+    return $object->getAttachmentsQuery()->execute();
+  }
+  public function setAttachments($attachments)
+  {
+    foreach ($attachments as $attachment) 
+    {
+      $this->addAttachment($attachment);
+    }
+  }
+  public function hasAttachments()
+  {
+    return ($this->getAttachments()->count() > 0);
+  }
+  public function hasAttachmentsOfType($type)
+  {
+    return ($this->getAttachmentsByType($type)->count() > 0);
+  }
+  public function addAttachment($attachment)
+  {
+    $object = $this->getInvoker();
+    $attachment->setObjectClass(get_class($object));
+    if(!$object['id'])
+    {
+      $object->save();
+    }
+    $attachment->setObjectId($object->getId());
+    $attachment->save();  
+  }
+  
+  public function getAttachmentsByType($type)
+  {
+    if(in_array($type, $this->_options['types']))
+    {
+      return Doctrine::getTable('Attachment')
+                  ->createQuery()
+                  ->where('object_class = ?', get_class($this->getInvoker()))
+                  ->andWhere('object_id = ?', $this->getInvoker()->getId())
+                  ->andWhere('type = ?', strtolower($type))
+                  ->execute();
+    }     
+    $table = strtolower($type) == 'other' ? 'Attachment' : sfInflector::classify($type.'_attachment');
+    return new Doctrine_Collection($table);
+  }
+  public function getSupportedAttachmentTypes()
+  {
+    return $this->getAttachableConfig('types');
+  }
+  public function getAttachableConfig($index = null)
+  {
+    if($index)
+    {
+      return $this->_options[$index];
+    }
+    return $this->_options;
+  }
+  public function getVideoAttachments()
+  {
+    return $this->getAttachmentsByType('Video');
+  }
+  public function getImageAttachments()
+  {
+    return $this->getAttachmentsByType('Image');
+  }
+  public function getDocumentAttachments()
+  {
+    return $this->getAttachmentsByType('Document');
+  }
+  public function getOtherAttachments()
+  {
+    return $this->getAttachmentsByType('Other');
+  }
+  public function getModelAttachments()
+  {
+    return $this->getAttachmentsByType('Model');
+  }
+
+  //This method doesn't work for some reason!
+  public function __call($method, $arguments)
+  {
+    // try
+    // {
+      // parent::__call($method, $arguments);
+    // }
+    // catch(Exception $e)
+    // {
+      foreach ($this->_options['types'] as $type) 
+      {
+        if(sfInflector::camelize('get_'.$type.'_attachments') == $method)
+        {
+          return $this->getAttachmentsByType($type);
+        }
+      }
+    // }
+  }
 }
